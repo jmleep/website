2a15eda97e246f8df64233d3efc83c4c3bf80b71
Jordan Leeper <j.m.leep@gmail.com>
2019-04-13 08:59:49 -0400

    switched to semantic ui

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import { Row, Col, Menu } from 'antd';
-import 'antd/dist/antd.css';
+import { Header, Image } from 'semantic-ui-react';
+import me from '../../static/me.jpg';
 import styles from './App.css';
 
 document.body.style.margin = 0;
 
 const App = () => (
   <div className={styles.app}>
-    <Row type="flex" justify="center" align="top">
-      <Col span={3} offset={5}>
-        <h1>Jordan Leeper</h1>
-      </Col>
-      <Col span={5} />
-    </Row>
-    <Row ype="flex" justify="center">
-      <Menu mode="horizontal">
-        <Menu.Item>Education</Menu.Item>
-        <Menu.Item>Experience</Menu.Item>
-        <Menu.Item>Projects</Menu.Item>
-      </Menu>
-    </Row>
+    <br />
+    <Image size="medium" src={me} circular centered />
+    <Header as="h1" icon textAlign="center">
+      <Header.Content>Jordan Leeper</Header.Content>
+    </Header>
     <br />
   </div>
 );
